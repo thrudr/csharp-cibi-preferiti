93b83a193f22fdeb86f96339b4a4adfb67e7fcee
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-14 16:06:56 +0200

    function-print-array; notes

--- CiboPreferito/Program.cs
+++ CiboPreferito/Program.cs
@@ -1,4 +1,45 @@
-﻿Console.WriteLine("Favorite Food");
+﻿Console.WriteLine("----------------Favorite Food---------------");
 
 //MAIN PROGRAM
-string[] arrayFavoriteFood = { "Apple", "Strawberry", "Meat", "Beer", "Wine", "Rise" };
+string[] arrayFavoriteFood = { "Apple", "Strawberry", "Meat", "Beer", "Wine", "Rise", };
+
+printArrayString(arrayFavoriteFood);
+
+
+
+
+
+//FUNCTION
+void printArrayString(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(" - " + array[i]);
+    }
+
+}
+
+
+
+
+
+
+
+//NOTES
+/*
+ * FOR WRITE IN CONSOLE
+Console.WriteLine(array[0]);
+
+
+ * FOR WRITE IN CONSOLE THE ARRAY LENGTH (NUMBER)
+Console.WriteLine(array.Length);
+
+
+ * FOR WRITE IN CONSOLE THE ARRAY ELEMENTS
+for (int i = 0; i < array; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+
+*/
